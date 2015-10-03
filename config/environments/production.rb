d8857b220f88c89567c7e5ac7e9468e36ea11b7b
Bob Graham <bob.graham@airwavesmusic.ca>
2015-10-02 17:51:42 -0700

    Added Amazon s3 again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+# Sets paperclip to upload images to amazons3
     config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
+    :storage => :s3,
+    :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
