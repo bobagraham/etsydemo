20a0a0ef665abd5ee4cf0085267aa4b96393c40a
Bob Graham <bob.graham@airwavesmusic.ca>
2015-10-04 17:51:13 -0700

    updated secret access key id

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,12 @@
 class Listing < ActiveRecord::Base
 	has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "/images/image.jpeg"
 	  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	   :storage => :s3,
-                :bucket => 'etsydemo11',
-        credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+	      :storage => :s3,
+		  :bucket => "etsydemo11",
+		  :s3_credentials => {
+		      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    		  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+		  }
+
 
 end
