eee51883af5ff7e3662475b765214aca8c7ba64e
Bob Graham <bob.graham@airwavesmusic.ca>
2015-10-04 17:52:58 -0700

    removed heroku and s3 code from model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,6 @@
 class Listing < ActiveRecord::Base
 	has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "/images/image.jpeg"
 	  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	      :storage => :s3,
-		  :bucket => "etsydemo11",
-		  :s3_credentials => {
-		      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    		  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-		  }
 
 
 end
